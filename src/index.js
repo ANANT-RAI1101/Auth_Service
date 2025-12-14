2cd6cce8fabe22112f437aadefbaec83ea8feb62
ANANT-RAI1101 <anantrai1101@gmail.com>
2025-12-14 17:23:33 +0530

    created project setup for Auth service and created database

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express=require("express")
+
+const {PORT}=require('./config/serverConfig');
+const bodyParser = require("body-parser");
+
+const SetupAndServer=async()=>{
+    const app=express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.listen(PORT,async()=>{
+        console.log(`server is running ${PORT}`);
+    });
+}
+
+SetupAndServer();
