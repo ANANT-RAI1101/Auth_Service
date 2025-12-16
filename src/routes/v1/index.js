abbd29224e083fc65f911ca20f4f48e4acc7fd6b
ANANT-RAI1101 <anantrai1101@gmail.com>
2025-12-16 17:09:16 +0530

    created role model and established many to many relation in user and role and synced db

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,10 @@
 const express =require("express");
 const UserController = require('../../controllers/user-controller');
+const {AuthRequestMiddlewares}=require('../../middlewares/index')
 const router=express.Router();
 
 router.post('/signup', UserController.create);
-router.post('/signIn',UserController.signIn);
+router.post('/signIn',AuthRequestMiddlewares.validateUserAuth,UserController.signIn);
 router.get('/isAuthenticated',UserController.isAuthenticated);
 router.get('/verify-email',UserController.verifyEmail);
 router.delete('/delete/:id',UserController.destroy);
